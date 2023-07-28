9359d4c4f6b766426cbb12bb73ca8306a425fee5
iberis2 <snow9402@gmail.com>
2023-07-28 13:28:23 +0900

    Feat: Pretendard 폰트 적용
    - index.html에 link 태그 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './\bpages/Home'
 import New from './\bpages/New'
+import './App.css'
 
 function App() {
   return (
