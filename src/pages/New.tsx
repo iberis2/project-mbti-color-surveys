76771faae43bad842ee8762c5b96986036459a5b
iberis2 <snow9402@gmail.com>
2023-07-28 10:23:54 +0900

    Feat: MBTI 선택 컴포넌트 완성
    - 2개씩 짝지어진 배열의 속성 중 1가지씩 클릭하면 MBTI 가 변경되는 컴포넌트 완성

--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -1,5 +1,35 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import MBTISelect from '../components/MBTISelect'
+
 function New() {
-  return <div>New</div>
+  const [formValue, setFormValue] = useState({
+    mbti: 'ESTJ',
+    colorCode: '#000000',
+  })
+
+  const handleChange = (type: string, value: string) => {
+    setFormValue(prev => {
+      return { ...prev, [type]: value }
+    })
+  }
+
+  return (
+    <div>
+      <h1>새 컬러 등록하기</h1>
+      <Link to='/'>
+        <img src='/images/x.svg' alt='취소' />
+      </Link>
+
+      <h2>MBTI</h2>
+      <MBTISelect value={formValue.mbti} onChange={newMBTI => handleChange('mbti', newMBTI)} />
+
+      <h2>컬러</h2>
+      <img src='/images/repeat.svg' alt='랜덤' />
+      <input name={formValue.colorCode} onChange={e => handleChange('colorCode', e.target.value)} />
+      <button type='button'>컬러 등록</button>
+    </div>
+  )
 }
 
 export default New
---
+++ src/components/MBTISelect.tsx
@@ -0,0 +1,85 @@
+const optionGroups = [
+  [
+    { value: 'E', label: '외향형' },
+    { value: 'I', label: '내향형' },
+  ],
+  [
+    { value: 'S', label: '감각형' },
+    { value: 'N', label: '직관형' },
+  ],
+  [
+    { value: 'T', label: '사고형' },
+    { value: 'F', label: '감정형' },
+  ],
+  [
+    { value: 'J', label: '판단형' },
+    { value: 'P', label: '인식형' },
+  ],
+]
+
+type MBTIOptionsProp = {
+  selected: boolean
+  label: string
+  value: string
+  onClick: () => void
+}
+
+function MBTIOption({ selected, label, value, onClick }: MBTIOptionsProp) {
+  const style = { fontWeight: selected ? 'bold' : 'normal' }
+
+  return (
+    <span style={style} onClick={onClick}>
+      {value} {label}
+    </span>
+  )
+}
+
+type MBTIOptionGroupProp = {
+  options: {
+    value: string
+    label: string
+  }[]
+  value: string
+  onChange: (value: string) => void
+}
+
+function MBTIOptionGroup({ options, value, onChange }: MBTIOptionGroupProp) {
+  return (
+    <div>
+      {options.map(option => (
+        <MBTIOption
+          key={option.value}
+          selected={option.value === value}
+          label={option.label}
+          value={option.value}
+          onClick={() => onChange(option.value)}
+        />
+      ))}
+    </div>
+  )
+}
+
+type MBTISelectProp = {
+  value: string
+  onChange: (value: string) => void
+}
+function MBTISelect({ value = 'ESTJ', onChange }: MBTISelectProp) {
+  const handleChangeAt = (val: string, position: number) => {
+    const nextValue = value.slice(0, position) + val + value.slice(position + 1)
+    onChange(nextValue)
+  }
+  return (
+    <div>
+      {optionGroups.map((options, index) => (
+        <MBTIOptionGroup
+          key={`${options[0].value} - ${options[1].value}`}
+          value={value[index]}
+          options={options}
+          onChange={val => handleChangeAt(val, index)}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default MBTISelect
