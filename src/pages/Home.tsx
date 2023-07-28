59ea750d8adca5f61285a92465cb7e46b7af6e60
iberis2 <snow9402@gmail.com>
2023-07-28 09:29:20 +0900

    Feat: mock 데이터 맵핑, 클릭시 해당 mbti 조회되도록 filter
    - 프로젝트 안에서만 json 데이터 참조가 가능해서 mock.json 위치를 src 폴더 안으로 이동

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,31 @@
+import { Link } from 'react-router-dom'
+import mock from '../mock.json'
+import { useState } from 'react'
+
 function Home() {
-  return <div>Home</div>
+  const [filter, setFilter] = useState('')
+  return (
+    <div>
+      <h1>MBTI별 좋아하는 컬러</h1>
+      {filter && (
+        <button type='button' onClick={() => setFilter('')}>
+          {filter}
+          <img src='/images/x.svg' alt='필터 삭제' />
+        </button>
+      )}
+      <div>
+        <Link to='/new'> + 새 컬러 등록하기</Link>
+      </div>
+      <ul>
+        {mock.map(item => (
+          <li key={item.id} onClick={() => setFilter(item.mbti)}>
+            {item.id}
+            {item.mbti} --- {item.colorCode}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Home
